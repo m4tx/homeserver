ede07475d0548d0990103d547eb5b1fa198d9aa4
Mateusz Maćkowski <m4tx@m4tx.pl>
2023-10-07 19:04:55 +0100

    More backup updates

---
+++ backup_cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+"${SCRIPT_DIR}"/update.sh
+"${SCRIPT_DIR}"/backup/forget.sh "$@"
---
+++ install_server.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+if [[ $EUID -ne 0 ]];
+then
+    exec sudo /bin/bash "$0" "$@"
+fi
+
+cat >/etc/systemd/system/backup-cleanup@.timer <<EOF
+[Unit]
+Description=Remove old Restic backups on schedule
+
+[Timer]
+OnCalendar=*-*-* 6:00:00
+Persistent=true
+RandomizedDelaySec=900
+
+[Install]
+WantedBy=timers.target
+EOF
+
+cat >/etc/systemd/system/backup-cleanup@.service <<EOF
+[Unit]
+Description=Remove old Restic backups
+
+StartLimitIntervalSec=1800
+StartLimitBurst=10
+
+[Service]
+Type=simple
+Nice=10
+User=restic
+Group=restic
+ExecStart=/srv/homeserver/backup/forget.sh /etc/%i.conf
+
+Restart=on-failure
+RestartSec=60s
+EOF
+
+systemctl daemon-reload
+systemctl enable --now backup-cleanup@backup.timer
