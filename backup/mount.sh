4f84c7db6d171e3001dc2693f1855f706a02e120
Mateusz Maćkowski <m4tx@m4tx.pl>
2026-04-03 15:05:45 +0200

    add mount script

---
+++ backup/mount.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [[ $EUID -ne 0 ]];
+then
+    exec sudo /bin/bash "$0" "$@"
+fi
+
+BACKUP_CONF_PATH=/etc/rustic/rustic.toml
+EXPECTED_PERMS="400"
+PERMS=$(stat -c '%a' "$BACKUP_CONF_PATH")
+
+if [[ "$PERMS" != "$EXPECTED_PERMS" ]]; then
+  echo "$BACKUP_CONF_PATH permissions are $PERMS instead of $EXPECTED_PERMS; please execute \`sudo chmod $EXPECTED_PERMS $BACKUP_CONF_PATH\` before running the backup script"
+  exit 1
+fi
+
+sudo -u rustic -HE rustic "$@"
